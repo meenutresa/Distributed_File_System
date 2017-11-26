3a32547633ea1007b088085e8900d8118fabc66a
meenutresa <meenuvincent1992@gmail.com>
2017-11-26 17:21:07 +0000

    lock server post method

--- Client.py
+++ Client.py
@@ -1,9 +1,9 @@
 import requests as r
 import json as j
 
-url = "http://localhost:8081/"
+url = "http://localhost:8082/"
 file_name = input("Enter the file path : ")
 url = url + str(file_name)
-response = r.get(url)
+response = r.post(url)
 #file_size = response.json()
 print("Response : ", response.text)
--- Lock_Server.py
+++ Lock_Server.py
@@ -9,7 +9,7 @@ urls = (
 
 class lock_server:
     #This gets the name of the file and check if it is locked or not
-    #if not locked, it locks the file
+    #if * is passed, then it displays all the files that are not locked.
     def GET(self, File_name):
         if not File_name:
             File_size = 'No input given'
@@ -34,15 +34,35 @@ class lock_server:
                     database = shelve.open("lock_files.dat")
                     lock = database[File_name]
                     if "NoLock" in lock:
-                        database[File_name] = 'Lock'
-                        return 'Locked the file given'
+                        return 'The file is not locked'
                     else:
-                        return 'The file is already locked by other user'
+                        return 'The file is locked by other user'
                 except KeyError as err:
                     error = "file not found"
                     return error
                 finally:
                     database.close()
+
+    def POST(self, File_name):
+        #post accpets the file name and if it is not locked, it locks the file.
+        if not File_name:
+            File_size = 'No input given'
+            return File_size
+        else:
+            try:
+                database = shelve.open("lock_files.dat")
+                lock = database[File_name]
+                if "NoLock" in lock:
+                    database[File_name] = 'Lock'
+                    return str(File_name) + ' is locked now'
+                else:
+                    return 'The file ' + str(File_name) +' is locked by other user'
+            except KeyError as err:
+                error = "file not found"
+                return error
+            finally:
+                database.close()
+
 
 if __name__ == "__main__":
     app = MyApplication.MyApplication(urls, globals())
